3ae6d32c751ac6c491a04a1bee7e90464adce572
Emma Mondul <emondul@gmail.com>
2022-02-21 15:28:29 -0800

    adding date

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,14 @@
-// $('.container')
-// $('#currentDay')
+var date = moment().format("MMMM Do YYYY");
+$('#currentDay').append(date);
 //currentHour
 
 // loop from 9 to 17
 for(var i = 9; i < 17; i++) {
+    var savedValueForHour = localStorage.getItem( "hour-" + i );
 
 }
+
+$('.container').on('click','button',function(event) {
+
+
+})
